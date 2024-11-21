b95e5cf0052ccf69442530cdeb8f06d622781e5c
vincemisayah <vincemisayah@gmail.com>
2024-11-21 01:41:37 -0600

    Update

--- app/commissionConfigs/invoiceLevel/SalesPersonCalculatedCommission.tsx
+++ app/commissionConfigs/invoiceLevel/SalesPersonCalculatedCommission.tsx
@@ -10,7 +10,7 @@ import {
     TableRow,
     Tooltip
 } from "@nextui-org/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { PiPercentLight } from "react-icons/pi";
 import { DeleteIcon, EditIcon, EyeIcon } from "@nextui-org/shared-icons";
 import { RxReset } from "react-icons/rx";
@@ -24,11 +24,44 @@ const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
 
 // @ts-ignore
 const SalesPersonCalculatedCommission = ({customerID, invoiceID, taskID, orderNumber, employeeID}) =>{
+    const divRef = useRef(null);
+    const messageIconDivRef = useRef(null);
+
+    const divRef2 = useRef(null);
+    const messageIconDivRef2 = useRef(null);
+
     useEffect(() => {
         const table = document.getElementById("tableId#" + employeeID);
         // @ts-ignore
         table.style.opacity = "1";
     }, [employeeID]);
+
+    useEffect(() => {
+        const handleClickOutside = (event: { target: any; }) => {
+            // @ts-ignore
+            if (divRef.current && !divRef.current.contains(event.target) // @ts-ignore
+                && (messageIconDivRef.current && !messageIconDivRef.current.contains(event.target))) {
+                const textAreaDiv = document.getElementById('taskNote#' + employeeID + '#taskId#' + taskID);
+                // @ts-ignore
+                if(!textAreaDiv.hidden) { // @ts-ignore
+                    textAreaDiv.hidden = true
+                }
+            }
+
+            if (divRef2.current && !divRef2.current.contains(event.target) // @ts-ignore
+                && (messageIconDivRef2.current && !messageIconDivRef2.current.contains(event.target))) {
+                const textAreaDiv = document.getElementById('salesNote#' + employeeID + '#taskId#' + taskID);
+                // @ts-ignore
+                if(!textAreaDiv.hidden) { // @ts-ignore
+                    textAreaDiv.hidden = true
+                }
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     const { data: calculatedCommissionInfo, error: calculatedCommissionInfoError } = useSWR(invoiceID > 0?
             "http://localhost:1118/invoiceCommissionService/customerlevel/calculatedInvoiceTaskCommission?" +
@@ -83,18 +116,18 @@ const SalesPersonCalculatedCommission = ({customerID, invoiceID, taskID, orderNu
                     <TableBody>
                         <TableRow
                             className={'border-b-small'}>
-                            <TableCell className={'text-[9pt] dark:text-[#dedfe1]'}>
+                            <TableCell className={'text-[10pt] dark:text-[#dedfe1]'}>
                                 {formatter.format(calculatedCommissionInfo.amount)}
                             </TableCell>
                             {/*<TableCell className={'text-[9pt] dark:text-[#dedfe1]'}>{calculatedCommissionInfo.taskRate}</TableCell>*/}
-                            <TableCell className={'text-[9pt] dark:text-[#dedfe1]'}>
+                            <TableCell className={'text-[10pt] dark:text-[#dedfe1]'}>
                                 <div className={"flex"}>
                                     <input type={"number"}
                                            defaultValue={calculatedCommissionInfo.taskRate}
-                                           className={"remove-arrow border-1 text-center w-full sm:w-16 rounded"}
+                                           className={"dark:bg-[#18181b] remove-arrow border-1 text-center w-full sm:w-16 rounded"}
                                     />
                                     <PiPercentLight className={"ml-1"} size={15} />
-                                    <div>
+                                    <div ref={messageIconDivRef}>
                                         <FaRegMessage
                                             color={clsx({
                                                 ["#06b6d4"]: (calculatedCommissionInfo.taskRateNote).length > 0
@@ -103,55 +136,30 @@ const SalesPersonCalculatedCommission = ({customerID, invoiceID, taskID, orderNu
                                             onClick={()=>showNote("taskNote#" + employeeID + "#taskId#" + taskID )}
                                         />
                                     </div>
-                                    <div id={"taskNote#" + employeeID + "#taskId#" + taskID} hidden={true}>
-                                        <div className={"bg-[#f4f4f5] dark:bg-[#4a4a50] absolute mt-3 z-10 rounded border-small border-default-200 dark:border-default-100 p-1 shadow-xl"}>
+                                    <div ref={divRef} id={"taskNote#" + employeeID + "#taskId#" + taskID} hidden={true}>
+                                        <div
+                                            className={"bg-[#f4f4f5] dark:bg-[#4a4a50] absolute mt-3 z-10 rounded border-small border-default-200 dark:border-default-100 p-1 shadow-xl"}>
                                             <textarea
                                                 id={"textAreaTaskNote#" + employeeID + "#taskId#" + taskID}
-                                                className={"text-[10pt] dark:bg-[#27272a] rounded border-small border-default-200 dark:border-default-100 p-2"}
+                                                className={"max-h-[120px] text-[10pt] dark:bg-[#27272a] rounded border-small border-default-200 dark:border-default-100 p-2"}
                                                 defaultValue={calculatedCommissionInfo.taskRateNote}
                                                 rows={4} maxLength={150}>
                                             </textarea>
                                         </div>
                                     </div>
-
-
-                                    {/*<Dropdown>*/}
-                                    {/*    <DropdownTrigger>*/}
-                                    {/*        <div>*/}
-                                    {/*            <FaRegMessage*/}
-                                    {/*                color={clsx({*/}
-                                    {/*                    ["#06b6d4"]: (calculatedCommissionInfo.taskRateNote).length > 0*/}
-                                    {/*                })}*/}
-                                    {/*                className={"ml-1 hover:cursor-pointer"}*/}
-                                    {/*            />*/}
-                                    {/*        </div>*/}
-
-
-                                    {/*    </DropdownTrigger>*/}
-                                    {/*    <DropdownMenu disabledKeys={["99"]} variant={"faded"}>*/}
-                                    {/*        <DropdownItem key={"99"} className={"ml-[25%]"}>*/}
-
-                                    {/*            <span*/}
-                                    {/*                className={"text-[11pt] text-black dark:text-[white] select-none"}>*/}
-                                    {/*                {calculatedCommissionInfo.taskRateNote}*/}
-                                    {/*            </span>*/}
-                                    {/*        </DropdownItem>*/}
-                                    {/*    </DropdownMenu>*/}
-                                    {/*</Dropdown>*/}
-
                                 </div>
                             </TableCell>
-                            <TableCell className={"text-[pt] dark:text-[#dedfe1]"}>
+                            <TableCell className={"text-[10pt] dark:text-[#dedfe1]"}>
                                 {formatter.format(calculatedCommissionInfo.taskCommissionDollarValue)}
                             </TableCell>
-                            <TableCell className={"text-[9pt] dark:text-[#dedfe1]"}>
+                            <TableCell className={"text-[10pt] dark:text-[#dedfe1]"}>
                                 <div className={"flex"}>
                                     <input type={"number"}
                                            defaultValue={calculatedCommissionInfo.salesPersonAssignedRate}
-                                           className={"remove-arrow border-1 text-center w-full sm:w-16 rounded"}
+                                           className={"dark:bg-[#18181b] remove-arrow border-1 text-center w-full sm:w-16 rounded"}
                                     />
                                     <PiPercentLight className={"ml-1"} size={15} />
-                                    <div>
+                                    <div ref={messageIconDivRef2}>
                                         <FaRegMessage
                                             color={clsx({
                                                 ["#06b6d4"]: (calculatedCommissionInfo.taskRateNote).length > 0
@@ -160,12 +168,12 @@ const SalesPersonCalculatedCommission = ({customerID, invoiceID, taskID, orderNu
                                             onClick={() => showNote("salesNote#" + employeeID + "#taskId#" + taskID)}
                                         />
                                     </div>
-                                    <div id={"salesNote#" + employeeID + "#taskId#" + taskID} hidden={true}>
+                                    <div ref={divRef2} id={"salesNote#" + employeeID + "#taskId#" + taskID} hidden={true}>
                                         <div
                                             className={"bg-[#f4f4f5] dark:bg-[#4a4a50] absolute mt-3 z-10 rounded border-small border-default-200 dark:border-default-100 p-1 shadow-xl"}>
                                             <textarea
                                                 id={"textAreaSalesNote#" + employeeID + "#taskId#" + taskID}
-                                                className={"text-[10pt] dark:bg-[#27272a] rounded border-small border-default-200 dark:border-default-100 p-2"}
+                                                className={"max-h-[120px] text-[10pt] dark:bg-[#27272a] rounded border-small border-default-200 dark:border-default-100 p-2"}
                                                 defaultValue={calculatedCommissionInfo.salesPersonAssignedRateNote}
                                                 rows={4} maxLength={150}>
                                             </textarea>
@@ -173,7 +181,7 @@ const SalesPersonCalculatedCommission = ({customerID, invoiceID, taskID, orderNu
                                     </div>
                                 </div>
                             </TableCell>
-                            <TableCell className={"text-[9pt] dark:text-[#dedfe1]"}>
+                            <TableCell className={"text-[10pt] dark:text-[#dedfe1]"}>
                                 {formatter.format(calculatedCommissionInfo.salesDollarValue)}
                             </TableCell>
                             <TableCell>
