6a6a584f4ccd1863b3e1e782ddedc567eaa10f71
vincemisayah <vincemisayah@gmail.com>
2024-12-22 15:36:14 -0600

    update

--- app/commissionConfigs/invoiceLevel/InvoiceLevelContent.tsx
+++ app/commissionConfigs/invoiceLevel/InvoiceLevelContent.tsx
@@ -11,55 +11,24 @@ import SearchInvoice from "@/app/commissionConfigs/invoiceLevel/SearchInvoice";
 const InvoiceLevelContent = () => {
     const [targetInvoiceNumber, setTargetInvoiceNumber] = useState(0);
 
-
-
     const handleNameChange = (newInvoiceID: SetStateAction<number>) => {
         const changeInvoiceBtn = document.getElementById('changeInvoiceBtn');
-        // @ts-ignore
-        if(changeInvoiceBtn.hidden){
+        if(changeInvoiceBtn !== null && changeInvoiceBtn.hidden){
             changeInvoiceBtn.hidden = false;
         }
-
         setTargetInvoiceNumber(newInvoiceID);
     };
 
     return (
         <>
             <div className={'flex gap-4'}>
-                {/*<div id={'searchInvoiceContainer'}  className={'border-1 rounded shadow-md text-center p-3 w-[20vw] min-w-fit'}>*/}
                 <div id={'searchInvoiceContainer'}
                      className={'border-1 h-fit rounded shadow-md text-center p-3 w-[100%]'}>
-                    {/*@ts-ignore*/}
-                    {/*<input id={'invoiceInputField'}*/}
-                    {/*    size={12}*/}
-                    {/*    type={'number'}*/}
-                    {/*    className={'remove-arrow text-center border-small bg-gray-100 dark:bg-[#27272a] rounded'}*/}
-                    {/*    placeholder={'Invoice Number'}*/}
-                    {/*    onKeyDown={(e) => {*/}
-                    {/*       if (e.key === "Enter")/!*@ts-ignore*!/*/}
-                    {/*           setTargetInvoiceNumber(e.target.value);*/}
-                    {/*    }}*/}
-                    {/*/>*/}
-                    {/*@ts-ignore*/}
-                    {/*<Button onPress={(e) => setTargetInvoiceNumber(document.getElementById('invoiceInputField').value)}*/}
-                    {/*    className={'ml-3 border-small shadow-sm'}*/}
-                    {/*    size={'sm'}*/}
-                    {/*    variant={'light'}>*/}
-                    {/*    <FaMagnifyingGlass color={'#71717a'}/>*/}
-                    {/*    Search*/}
-                    {/*</Button>*/}
-                    {/*<Spacer y={2}/>*/}
-                    {/*<ShowSearchResults*/}
-                    {/*    onInvoiceIdChange={handleNameChange}*/}
-                    {/*    searchInputValue={targetInvoiceNumber}/>*/}
-
                     <SearchInvoice onInvoiceIdChange={handleNameChange}/>
                 </div>
-
                 <div>
                     <DisplayInvoice invoiceNumber={targetInvoiceNumber} />
                 </div>
-
             </div>
         </>
     )
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,14 +9,11 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Image } from "@nextui-org/react";
+import {Popover, PopoverTrigger, PopoverContent, Button} from "@nextui-org/react";
 import React from "react";
-import { ChevronDown, Eye } from "@nextui-org/shared-icons";
-import { FcDataConfiguration } from "react-icons/fc";
 import { ThemeSwitch } from "@/components/theme-switch";
+import { FcDataConfiguration } from "react-icons/fc";
 
 
 export const Navbar = () => {
@@ -34,158 +31,66 @@ export const Navbar = () => {
 
     };
 
+    const PopoverMenuItem = ({menuObj}:any) =>{
+        return (
+            <div>
+                <div className={"flex items-center space-x-2"}>
+                    <FcDataConfiguration size={23} />
+                    <Link href={menuObj.linkPath}
+                        size={'sm'}
+                        className={"font-semibold hover:cursor-pointer"}>
+                        {menuObj.title}
+                    </Link>
+                </div>
+                <div className={"ml-12"}>
+                    <p className={'text-[13px] max-w-56'}>
+                        {menuObj.description}
+                    </p>
+                </div>
+            </div>
 
-  return (
-      <NextUINavbar>
-          <NavbarBrand as="li" className="gap-3 max-w-fit">
-              {/*<NextLink className="flex justify-start items-center gap-1" href="/home">*/}
-                  <p className="text-2xl font-extrabold text-inherit text-emerald-600 antialiased">FISHER</p>
-              {/*</NextLink>*/}
-          </NavbarBrand>
-          <NavbarContent className="hidden sm:flex gap-4" justify="center">
-              <Dropdown>
-                  <NavbarItem>
-                      <DropdownTrigger>
-                          <Button
-                              disableRipple
-                              className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                              radius="sm"
-                              variant="light"
-                          >
-                              Configurations
-                          </Button>
-                      </DropdownTrigger>
-                  </NavbarItem>
-                  <DropdownMenu
-                      className="w-[340px]"
-                      itemClasses={{
-                          base: "gap-4",
-                      }}
-                  >
-                      <DropdownItem
-                          href={menuItems.CUSTOMER.linkPath}
-                          startContent={<FcDataConfiguration size={23}/>}
-                          description={menuItems.CUSTOMER.description}>
-                          <span className={'menu-title font-bold text-cyan-500'}>
-                              {menuItems.CUSTOMER.title}</span>
-                      </DropdownItem>
-                      <DropdownItem
-                          startContent={<FcDataConfiguration size={23} />}
-                          description={menuItems.JOB.description}>
-                           <span className={"font-bold text-cyan-500"}>
-                                {menuItems.JOB.title}</span>
-                      </DropdownItem>
-                      <DropdownItem
-                          href={menuItems.INVOICE.linkPath}
-                          startContent={<FcDataConfiguration size={23} />}
-                          description={menuItems.INVOICE.description}>
-                          <span className={"font-bold text-cyan-500"}>
-                            {menuItems.INVOICE.title}</span>
-                      </DropdownItem>
-                  </DropdownMenu>
-              </Dropdown>
-              <NavbarItem isActive>
-                  <Link href="/reports" aria-current="page">
-                      Reports
-                  </Link>
-              </NavbarItem>
-          </NavbarContent>
-          <NavbarContent justify="end">
-              <NavbarItem className="hidden sm:flex gap-2">
-                  <ThemeSwitch />
-              </NavbarItem>
-              <NavbarItem className="hidden lg:flex">
-                  <Link href="#">Login</Link>
-              </NavbarItem>
-              <NavbarItem>
-                  <Button as={Link} color="primary" href="#" variant="flat">
-                      Sign Up
-                  </Button>
-              </NavbarItem>
-          </NavbarContent>
+        );
+    }
+
+
+    return (
+        <NextUINavbar>
+            <NavbarBrand as="li" className="gap-3 max-w-fit">
+                <p className="text-2xl font-extrabold text-inherit text-emerald-600 antialiased">FISHER</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem isActive>
+                    <Popover radius={'sm'} shadow={'md'} placement={'bottom-start'}>
+                        <PopoverTrigger>
+                            <span className={'text-md hover:cursor-pointer'}>Configurations</span>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <div className={"px-1 py-2"}>
+                                <PopoverMenuItem menuObj={menuItems.CUSTOMER}/>
+                                <PopoverMenuItem menuObj={menuItems.INVOICE}/>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="/reports" aria-current="page">
+                        Reports
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden sm:flex gap-2">
+                    <ThemeSwitch />
+                </NavbarItem>
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
       </NextUINavbar>
-
-
-
-
-
-
-
-
-
-
-
-    // <NextUINavbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
-    //     <NavbarContent className="basis-1/5 sm:basis-full">
-    //         <NavbarBrand as="li" className="gap-3 max-w-fit">
-    //             <NextLink className="flex justify-start items-center gap-1" href="/home">
-    //                 <p className="text-2xl font-extrabold text-inherit text-emerald-600 antialiased">FISHER</p>
-    //             </NextLink>
-    //         </NavbarBrand>
-    //         {/*<NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />*/}
-    //
-    //         <ul className="hidden lg:flex gap-4 justify-start ml-2">
-    //             {/*<NavbarItem >*/}
-    //             {/*    <NextLink color="foreground" href={"/configurations"} aria-label={isMenuOpen ? "Close menu" : "Open menu"}*/}
-    //             {/*              className={clsx(linkStyles({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium",)}>*/}
-    //             {/*        Configurations*/}
-    //             {/*    </NextLink>*/}
-    //             {/*</NavbarItem>*/}
-    //             <NavbarItem >
-    //                 <NextLink color="foreground" href={"/reports"}
-    //                           className={clsx(linkStyles({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium",)}>
-    //                     Reports
-    //                 </NextLink>
-    //             </NavbarItem>
-    //         </ul>
-    //
-    //         {/*<ul className="hidden lg:flex gap-4 justify-start ml-2">*/}
-    //         {/*    {siteConfig.navItems.map((item) => (*/}
-    //         {/*        <NavbarItem key={item.href}>*/}
-    //         {/*            <NextLink*/}
-    //         {/*                className={clsx(*/}
-    //         {/*                    linkStyles({ color: "foreground" }),*/}
-    //         {/*                    "data-[active=true]:text-primary data-[active=true]:font-medium",*/}
-    //         {/*                )}*/}
-    //         {/*                color="foreground"*/}
-    //         {/*                href={item.href}*/}
-    //         {/*            >*/}
-    //         {/*                {item.label}*/}
-    //         {/*            </NextLink>*/}
-    //         {/*        </NavbarItem>*/}
-    //         {/*    ))}*/}
-    //         {/*</ul>*/}
-    //     </NavbarContent>
-    //
-    //     <NavbarContent
-    //         className="hidden sm:flex basis-1/5 sm:basis-full"
-    //         justify="end">
-    //         <NavbarItem className="hidden sm:flex gap-2">
-    //
-    //             <ThemeSwitch />
-    //         </NavbarItem>
-    //
-    //
-    //     </NavbarContent>
-    //
-    //     <NavbarMenu>
-    //         {menuItems.map((item, index) => (
-    //             <NavbarMenuItem key={`${item}-${index}`}>
-    //                 <Link
-    //                     className="w-full"
-    //                     color={
-    //                         index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-    //                     }
-    //                     href="#"
-    //                     size="lg"
-    //                 >
-    //                     {item}
-    //                 </Link>
-    //             </NavbarMenuItem>
-    //         ))}
-    //     </NavbarMenu>
-    //
-    // </NextUINavbar>
-
-);
+    );
 };
